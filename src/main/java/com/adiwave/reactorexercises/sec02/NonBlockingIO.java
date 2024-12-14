3df0a26cd4fa4b26cba70309d54c07a9f4eb48c3
Davide Listello <davide.listello@gmail.com>
2024-12-14 18:09:54 +0100

    improved flux example

--- src/main/java/com/adiwave/reactorexercises/sec02/NonBlockingIO.java
+++ src/main/java/com/adiwave/reactorexercises/sec02/NonBlockingIO.java
@@ -6,19 +6,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
+
 public class NonBlockingIO {
 
     private static final Logger logger = LoggerFactory.getLogger(NonBlockingIO.class);
 
     public static void main(String[] args) {
-        ExternalServiceClient externalServiceClient = new ExternalServiceClient();
+        // service not available !!
+        // TODO: Pay Idea Subscription 1 year on 15.01.2025
+        /* ExternalServiceClient externalServiceClient = new ExternalServiceClient();
 
         for(int i = 1; i <= 15; i++) {
             externalServiceClient.getProductName(i)
                     .subscribe(Util.subscriber(""));
 
         }
-
+        */
         FileServiceImpl fileService = new FileServiceImpl();
         fileService.write("new-file.txt", "Hello World !! \r\n My Friend Dexter !")
                 .subscribe(Util.subscriber("write"));
@@ -28,8 +31,18 @@ public class NonBlockingIO {
                 .subscribe(Util.subscriber("delete"));
 
 
-        Flux.just(1, 2, 3, 4, 5, "READY ! GO !")
-                        .subscribe(Util.subscriber("flux"));
+        Flux<Integer> flux = Flux.just(1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+
+        flux.filter(i -> i % 2 == 0)
+                .subscribe(Util.subscriber("sub1"));
+        flux.filter(i -> i > 7)
+            .map(i -> "a"+i+"b")
+                .subscribe(Util.subscriber("sub2"));
+        flux.filter(i -> i % 3 == 0)
+                .subscribe(Util.subscriber("sub3"));
+        flux.filter( i -> i % 5 == 0)
+                .subscribe(Util.subscriber("sub4"));
+
 
         Util.sleepSeconds(2);
     }
